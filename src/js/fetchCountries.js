5e9d36a33cfa6e86381794e872bc82326d661fb1
GromovVv <pavel.kuts@gmail.com>
2021-03-22 21:19:14 +0200

    correct - 3

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,8 +3,13 @@ import { errorFetch } from './notifications.js';
 function fetchCountry(searchQuery) {
   const url = `https://restcountries.eu/rest/v2/name/${searchQuery}`;
   return fetch(url)
-    .then(response => response.json())
-    .then(data => data)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+
+    return Promise.reject('fetch have not worked: error' + response.status);
+  })
     .catch(() => errorFetch());
 }
 export default fetchCountry;
